6d0481efe34755cab212cd5bac1b2e14241fef0e
SHagege <solalhagege@gmail.com>
2019-01-23 20:49:21 -0800

    Added a thread that construct a memory pool every X seconds, bug fixes, refactoring

--- blackbox/blockchain.py
+++ blackbox/blockchain.py
@@ -1,6 +1,10 @@
 from block import Block
 import datetime as date
+from smapi import Smapi
+from smdata import Smdata
+import threading
 import twitter
+import instagram
 import sys
 import os
 
@@ -13,53 +17,54 @@ class Blockchain:
     nChain: The list containing all the blocks
     MAX_BLOCK_SIZE: The maximum block size of a block, here set to 1MB
     currentFileIndex: The current index of the files where the blocks are saved
-    api: Twitter's api to connect to Twitter and fetch tweets
+    mempool: Temporary list of unconfirmed social media data, keep track of transactions
+    that are known to the network but are not yet included in the blockchain
   """
   def __init__(self):
     self.blockHeight = 1
     self.nChain = []
     self.MAX_BLOCK_SIZE = 1024
     self.currentFileIndex = 0
-    self.api = ""
+    self.mempool = []
+    self.sm = Smapi()
     self.genesis(Block(0))
-    self.configure()
     self.run()
-
-  def configure(self):
-    """Read the config file to get user's credentials and connect to Twitter's API"""
-    fp = open('./config.txt')
-    lines = fp.read().split("\n")
-    fp.close()
-    with open('./config.txt') as fp:
-      lines = fp.readlines()
-    self.api = twitter.Api(consumer_key=lines[0].split('=')[1].strip('\n').strip("'"), 
-      consumer_secret=lines[1].split('=')[1].strip('\n').strip("'"),
-      access_token_key=lines[2].split('=')[1].strip('\n').strip("'"), 
-      access_token_secret=lines[3].split('=')[1].strip('\n').strip("'"))
 
   def genesis(self, bGen):
     """Create the genesis block of the blockchain with nothing in it"""
     bGen.timestamp = date.datetime.now()
     bGen.previous_hash = 0
     bGen.data = "Genesis Block"
-    self.nChain.append(bGen)
-    print "Block data: " + bGen.data
+    self.add_block(bGen)
     
   def run(self):
+    """"""
+    self.mempoolHandling()
     while True:
       self.fill_block(Block(self.blockHeight))
 
-  def fill_block(self, bNew):
-    """Fetch the tweets from Twitter's API"""
-    t = self.api.GetUserTimeline(screen_name="WeiRdCroissant", count=2)
+  def mempoolHandling(self):
+    """Starts a new thread that will handle the memory pool, putting inside it every X seconds Y 
+    tweets from a social media account"""
+    threading.Timer(14.0, self.mempoolHandling).start()
+    t = self.sm.apiTwitter.GetUserTimeline(screen_name="NESbot_feed", count=10)
     tweets = [i.AsDict() for i in t]
     for t in tweets:
-      if (bNew.BLOCK_SIZE + sys.getsizeof(t['text'].encode('utf-8')) < self.MAX_BLOCK_SIZE):
-        bNew.BLOCK_SIZE += sys.getsizeof(t['text'].encode('utf-8'))
-        bNew.data.append((t['text'].encode('utf-8')))
+      self.mempool.append(Smdata(t['text'].encode('utf-8')))
+
+  def fill_block(self, bNew):
+    """Fetch the tweets from Twitter's API"""
+    while self.mempool:
+      smdata = self.mempool.pop(0)
+      if (bNew.BLOCK_SIZE + sys.getsizeof(smdata) < self.MAX_BLOCK_SIZE):
+        bNew.data.append(smdata.data)
+        bNew.BLOCK_SIZE += sys.getsizeof(smdata.data)
+        if not self.mempool:
+          self.add_block(bNew)
       else:
+        self.mempool.append(smdata)
         self.add_block(bNew)
-    self.add_block(bNew)
+        self.fill_block(Block(self.blockHeight))
 
   def add_block(self, bNew):
     """Block is full, meaning it needs to be mined and added to the blockchain"""
@@ -71,7 +76,9 @@ class Blockchain:
     print "Block's hash: " + bNew.hash
 
   def get_lastBlock(self):
-    return self.nChain[-1]
+    if self.nChain:
+      return self.nChain[-1]
+    return None
 
   def save_blocks(self, bNew):
     """Save the new blocks in a blk*.dat file"""
