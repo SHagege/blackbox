a3bf9f2e1347a6b24afcc9e7daf742158a1642a2
SHagege <solalhagege@gmail.com>
2019-02-10 20:13:37 -0800

    Add welcome and parameters

--- blackbox/main.py
+++ blackbox/main.py
@@ -1,10 +1,26 @@
-#!/usr/bin/env python
+import datetime
+import argparse
 
-from block import Block
-from blockchain import Blockchain
+from termcolor import colored
+
+from .blockchain import Blockchain
+
+parser = argparse.ArgumentParser(description='Blackbox "Alpha One" (v0.1.0-release)')
+parser.add_argument("--daemon", required=True, help='The daemon you want to connect to')
+parser.add_argument("--port", required=False, help='The port associated with the daemon')
+opts = parser.parse_args()
+
+def welcome():
+    print(str(datetime.datetime.now()) + "\nThis is the command line blackbox project.\nBlackbox 'Alpha One' (v0.1.0-release)")
+    print(colored("Specify social media name (e.g., jack)", "yellow"))
+    account = input('> Social media name (or Ctrl-C to quit): ')
+    print(colored("You chose " + account + """\n******************************************************************************\n""" +
+    "Mining process starting", "green"))
+    return account
 
 def main():
-    Blockchain()
+    account = welcome()
+    Blockchain(account, opts.port, opts.daemon)
     
 if __name__ == "__main__":
     main()
