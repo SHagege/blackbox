04e4876a8eff92933730ee6dd1c1e1f31b5cc9fc
SHagege <solalhagege@gmail.com>
2019-03-13 19:27:47 -0700

    Enable basic search for block content

--- explorer/blocks/views.py
+++ explorer/blocks/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from .models import BlockModel
+
+def block_detailed_view(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('search', None)
+        try:
+            if user_input.startswith("0000"):
+                block = BlockModel.objects.get(block_hash=user_input)
+                info = {
+                    'height': block.block_height
+                }
+            else:
+                block = BlockModel.objects.get(id=user_input)
+                info = {
+                    'hash': block.block_hash
+                }
+            return render(request, "block/detail.html", info)
+        except ObjectDoesNotExist:
+            return render(request, 'home.html') 
